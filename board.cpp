1465d94832b352a336c52afa625daa5506d1c1a7
soomyy <aaali@ualberta.ca>
2020-11-12 02:24:58 -0700

    adding stones to board after mouse click

--- board.cpp
+++ board.cpp
@@ -1,5 +1,5 @@
 /*************************************/
-/* A Simple OpenGL Example           */
+/* A Hex program using openGL & C++  */
 /*************************************/
 
 // this code just creates a window and draws three rectangles in it
@@ -10,7 +10,7 @@
 #include <iostream>
 #include <cassert>
 #include <cstdlib>
-
+#include <math.h>
 #include <fstream>
 #include <memory>
 #include <GL/freeglut.h>
@@ -19,6 +19,11 @@
 
 using namespace std;
 
+int x, y;
+bool check;
+int turn = 1;
+
+/*
 // set up projection
 void init(){
     glClearColor(0.0, 0.0, 0.0, 0.0); // clear -> black R,G,B,A = 0 (A=alpha channel)
@@ -30,110 +35,170 @@ void init(){
     glMatrixMode(GL_MODELVIEW);
 
     // all matrix operations that follow apply to modelview matrix
+}*/
+
+
+/*
+    Utility function to draw the circle
+*/
+void DrawCircle1(float cx, float cy, float r, int num_segments) 
+{   
+    glColor3f(0.0, 1.0, 0.0);
+    turn = 2;
+   
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < num_segments; i++)   
+    {
+        float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);//get the current angle 
+        float x = r * cosf(theta);//calculate the x component 
+        float y = r * sinf(theta);//calculate the y component 
+        glVertex2f(x + cx, y + cy);//output vertex 
+    }
+    glEnd();
+}
+
+void DrawCircle2(float cx, float cy, float r, int num_segments) 
+{   
+   
+    
+    glColor3f(0.0, 0.0, 1.0);
+    turn = 1;
+    
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < num_segments; i++)   
+    {
+        float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);//get the current angle 
+        float x = r * cosf(theta);//calculate the x component 
+        float y = r * sinf(theta);//calculate the y component 
+        glVertex2f(x + cx, y + cy);//output vertex 
+    }
+    glEnd();
 }
 
 // display content (called automatically in main loop)
 void display(){
     //glClear(GL_COLOR_BUFFER_BIT);
     //glColor3f(1.0, 1.0, 1.0);
-    glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);       // clear color buffer
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glColor3f(0.0, 0.0, 0.0);
-
-    // draw the bottom half of the board
-    double startx = 0.0;
-    double starty = 0.0;
-    for(int i=10; i>0; i--){
-        if(i==10){ startx = -45.0; starty = 5.0;}
-        else if(i==9){ startx = -40.0; starty = 0.0;}
-        else if(i==8){ startx = -35.0; starty = -5.0;}
-        else if(i==7){ startx = -30.0; starty = -10.0;}
-        else if(i==6){ startx = -25.0; starty = -15.0;}
-        else if(i==5){ startx = -20.0; starty = -20.0;}
-        else if(i==4){ startx = -15.0; starty = -25.0;}
-        else if(i==3){ startx = -10.0; starty = -30.0;}
-        else if(i==2){ startx = -5.0; starty = -35.0;}
-        else if(i==1){ startx =  0.0; starty = -40.0;}
-
-        int count = 0;
-        for(int j=0; j<i; j++){
-            glBegin(GL_POLYGON); 
-            glVertex3f(startx+count, starty, 0.0);
-            glVertex3f(startx-5.0+count, starty-2.5, 0.0);
-            glVertex3f(startx-5.0+count, starty-5.0, 0.0);
-            glVertex3f(startx+0.0+count, starty-7.5, 0.0);
-            glVertex3f(startx+5.0+count, starty-5.0, 0.0);
-            glVertex3f(startx+5.0+count, starty-2.5, 0.0);
-            glEnd(); 
-            count+=10;
-        }
-    }
-
-    // draw the upper half
-    for(int i=9; i>0; i--){
-        if(i==9){ startx = -40.0; starty = 10.0;}
-        else if(i==8){ startx = -35.0; starty = 15.0;}
-        else if(i==7){ startx = -30.0; starty = 20.0;}
-        else if(i==6){ startx = -25.0; starty = 25.0;}
-        else if(i==5){ startx = -20.0; starty = 30.0;}
-        else if(i==4){ startx = -15.0; starty = 35.0;}
-        else if(i==3){ startx = -10.0; starty = 40.0;}
-        else if(i==2){ startx = -5.0; starty = 45.0;}
-        else if(i==1){ startx =  0.0; starty = 50.0;}
-
-        int count = 0;
-        for(int j=0; j<i; j++){
-            glBegin(GL_POLYGON); 
-            glVertex3f(startx+count, starty, 0.0);
-            glVertex3f(startx-5.0+count, starty-2.5, 0.0);
-            glVertex3f(startx-5.0+count, starty-5.0, 0.0);
-            glVertex3f(startx+0.0+count, starty-7.5, 0.0);
-            glVertex3f(startx+5.0+count, starty-5.0, 0.0);
-            glVertex3f(startx+5.0+count, starty-2.5, 0.0);
-            glEnd(); 
-            count+=10;
-        }
-    }
-
-    /*
-    // the blue line
+    //glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
     //glClear(GL_COLOR_BUFFER_BIT);       // clear color buffer
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glColor3f(0.0, 1.0, 0.0);
-    startx = -10;
-    starty = 50;
-    double county=0;
-    double countx = 0;
-    for(int i=0; i<10; i++){
-        glVertex3f(startx, starty, 0.0);
-        glVertex3f(startx-countx, starty-county, 0.0);
-        countx -=10;
-        county -=2.5;
-        startx += countx;
-        starty += county;
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glColor3f(1.0, 0.0, 0.0);
+
+
+    double start_x = 0.0;
+    double start_y = 0.0;
+    for(int i=10; i>0; i--){
+        if(i==10){ start_x = 20.0; start_y = 185.0;}
+        if(i==9) { start_x = 40.0; start_y = 205;}
+        if(i==8 ){ start_x = 60.0; start_y = 225;}
+        if(i==7 ){ start_x = 80.0; start_y = 245;}
+        if(i==6 ){ start_x = 100.0; start_y = 265;}
+        if(i==5 ){ start_x = 120.0; start_y = 285;}
+        if(i==4 ){ start_x = 140.0; start_y = 305;}
+        if(i==3 ){ start_x = 160.0; start_y = 325;}
+        if(i==2 ){ start_x = 180.0; start_y = 345;}
+        if(i==1 ){ start_x = 200.0; start_y = 365;}
+
+        double increment_x = 0;
+        double increment_y = 0;
+        for(int j=0; j<i; j++){
+            glBegin(GL_POLYGON); 
+            glVertex3f(start_x-0+increment_x, start_y, 0.0);
+            glVertex3f(start_x-20+increment_x, start_y+10, 0.0);
+            glVertex3f(start_x-20+increment_x, start_y+20, 0.0);
+            glVertex3f(start_x+increment_x, start_y+30, 0.0);
+            glVertex3f(start_x+20+increment_x, start_y+20, 0.0);
+            glVertex3f(start_x+20+increment_x, start_y+10, 0.0);
+            increment_x += 40;
+            
+            glEnd();
+        }
+    } 
+
+    // draw top half of the board
+    for(int i=9; i>0; i--){
+        if(i==9) { start_x = 40.0; start_y = 165;}
+        if(i==8 ){ start_x = 60.0; start_y = 145;}
+        if(i==7 ){ start_x = 80.0; start_y = 125;}
+        if(i==6 ){ start_x = 100.0; start_y = 105;}
+        if(i==5 ){ start_x = 120.0; start_y = 85;}
+        if(i==4 ){ start_x = 140.0; start_y = 65;}
+        if(i==3 ){ start_x = 160.0; start_y = 45;}
+        if(i==2 ){ start_x = 180.0; start_y = 25;}
+        if(i==1 ){ start_x = 200.0; start_y = 05;}
+
+        double increment_x = 0;
+        double increment_y = 0;
+        for(int j=0; j<i; j++){
+            glBegin(GL_POLYGON); 
+            glVertex3f(start_x-0+increment_x, start_y, 0.0);
+            glVertex3f(start_x-20+increment_x, start_y+10, 0.0);
+            glVertex3f(start_x-20+increment_x, start_y+20, 0.0);
+            glVertex3f(start_x+increment_x, start_y+30, 0.0);
+            glVertex3f(start_x+20+increment_x, start_y+20, 0.0);
+            glVertex3f(start_x+20+increment_x, start_y+10, 0.0);
+            increment_x += 40;
+            
+            glEnd();
+        }
+    } 
+
+
+    if(check){
+        if(turn == 1){
+            DrawCircle1(x, y, 10, 15);
+        }
+        else if (turn == 2){
+            DrawCircle2(x,y, 10, 15);
+        }
     }
-    */
     glFlush();
     glutSwapBuffers();             // bring draw buffer to front
 }
 
+// Mouse click
+void onMouse(int button, int state, int mousex, int mousey){
+    
+    if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+        check = true;
+        x = mousex;
+        y = mousey;
+        cout << "left clicked down\n";
+        
+    }
+    glutPostRedisplay();
+}
 
-int main (int argc, char **argv)
+
+/*
+    Function to reshape
+*/
+void Reshape(int x, int y)
 {
+    glViewport(0, 0, x, y);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, x, y, 0, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+
+int main (int argc, char **argv){
     //LoadMap("map.bmp");
-  // setup window
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); // double buffering, rbg mode
-  glutInitWindowSize(400, 400);
-  glutInitWindowPosition(0, 0);
-  glutCreateWindow("Hex Board");
-  init();
+    // setup window
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); // double buffering, rbg mode
+    glutInitWindowSize(400, 400);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Hex Board");
+    glutMouseFunc(onMouse);
+    //init();
 
-  // register call-back function
-  glutDisplayFunc(display);
+    // register call-back function
+    glutReshapeFunc(Reshape);
+    glutDisplayFunc(display);
 
-  // glut takes over - calling display() repeatedly
-  glutMainLoop();
-  return 0;
+    // glut takes over - calling display() repeatedly
+    glutMainLoop();
+    return 0;
 }
